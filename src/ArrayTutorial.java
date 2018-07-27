f61dc696719358ee8d28a88a37f3577de9a0a432
praneet <vasu6657@gmail.com>
2018-07-26 19:43:03 -0700

    Merge branch 'master' of https://github.com/piddamsetty/library
    # Conflicts:
    #	src/ArrayTutorial.java

--- src/ArrayTutorial.java
+++ src/ArrayTutorial.java
@@ -8,15 +8,36 @@ import java.util.Random;
 public class ArrayTutorial {
 
     public static void main(String[] args) {
-       Integer[] testData = generateData(100);
-       printArray(testData);
-       smallestElementInArray(testData);
-       biggestElementInArray(testData);
-       sortArray(testData);
-       System.out.println("Sorted Array (Ascending Order): ");
-       printArray(testData);
-       System.out.println("Sorted Array (Descending Order): ");
-       reverseArray(testData);
+//        wordCounter("Hello my name defines who who my Hello person is Hello");
+        wordCounter("Hello my name defines who who my Hello person is Hello Hello my name defines who who my Hello person is Hello");
+//        charBreakdown("Praneetsai Iddamsetty");
+////       Integer[] testData = generateData(10);
+////       printArray(testData);
+//////       smallestElementInArray(testData);
+//////       biggestElementInArray(testData);
+////       sortArray(testData);
+////       System.out.println("Sorted Array (Ascending Order): ");
+////       printArray(testData);
+////       System.out.println("Sorted Array (Descending Order): ");
+////       reverseArray(testData);
+////        printArray(testData);
+//          Integer[] testData = {2, 5, 2, 6, 3, 2, 2, 4, 67, 2};
+//          Integer[] uniqueData = uniqueElementOfArray(testData);
+//          printArray(uniqueData);
+//
+//          char a = 98;
+//          System.out.println(a);
+//
+////        String s = "abcdefghijklmnopqrstuvwxyz";
+////        char[] a = s.toCharArray();
+////        char[] b = s.toUpperCase().toCharArray();
+////        for(int i =0 ;i<a.length;++i) {
+////            System.out.print((int) a[i] +" ");
+////        }
+////        System.out.println();
+////        for(int i =0 ;i<b.length;++i) {
+////            System.out.print((int) b[i] +" ");
+////        }
 
     }
 
@@ -143,7 +164,8 @@ public class ArrayTutorial {
         return null;
     }
 
-    public static Integer[] sortArray(Integer[] arr) {
+
+    public static void sortArray(Integer[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = i+1; j < arr.length; j++){
@@ -157,7 +179,6 @@ public class ArrayTutorial {
             }
         }
 
-        return arr;
     }
 
     public static void reverseArray(Integer[] arr) {
@@ -166,16 +187,120 @@ public class ArrayTutorial {
             arr[i] = arr[arr.length - (i+1)];
             arr[arr.length - (i+1)] = temp;
         }
-
-        printArray(arr);
-
     }
 
 
     public static Integer[] uniqueElementOfArray(Integer[] arr){
-
-        return null;
+        int firstPointer = 0;
+        int lastPointer = arr.length-1;
+        for (firstPointer = 0; firstPointer < lastPointer; firstPointer++) {
+            for (int i = 0; i < firstPointer; i++){
+                if(arr[i] != arr[firstPointer]) {
+                    continue;
+                } else {
+                    int temp = arr[firstPointer];
+                    while(arr[firstPointer]==arr[lastPointer] && lastPointer>0) {
+                        --lastPointer;
+                    }
+                    arr[firstPointer] = arr[lastPointer];
+                    arr[lastPointer] = temp;
+                    lastPointer--;
+                    break;
+                }
+            }
+        }
+        Integer[] uniqueData = new Integer[lastPointer];
+        for (int i = 0; i < lastPointer; i++) {
+            uniqueData[i] = arr[i];
+        }
+        return uniqueData;
     }
+
+    public static void charBreakdown (String a) {
+
+        char[] charArray =  a.toCharArray();
+        int[] asciiMatch = new int[52];
+        for (char character: charArray) {
+            if(character==' ') {
+                continue;
+            }
+            int matchReference = (int) character;
+            if(matchReference < 91) {
+              asciiMatch[matchReference-65]++;
+            } else {
+                asciiMatch[matchReference-71]++;
+            }
+
+        }
+
+        for(int i = 0; i < asciiMatch.length; i++){
+            if(asciiMatch[i] == 0){
+                continue;
+            }
+            char acciiChar = 0;
+            if(i<26) acciiChar= (char) (i+65);
+            else acciiChar=  (char) (i+71);
+            System.out.println(acciiChar+": "+ asciiMatch[i]);
+        }
+        System.out.println();
+    }
+
+    public static void wordCounter(String stringOfWords) {
+        int firstPointer = 0;
+        int lastPointer = 0;
+        String[] wordBreakdown = stringOfWords.split(" ");
+        lastPointer = wordBreakdown.length;
+        for (firstPointer = 0; firstPointer < lastPointer; firstPointer++) {
+            for(int i = 0; i < firstPointer; i++){
+                if (!wordBreakdown[i].equals(wordBreakdown[firstPointer]) ) {
+                   continue;
+
+                } else {
+                    System.out.println("Before while: " + firstPointer + " & " +lastPointer);
+                    String temp = wordBreakdown[firstPointer];
+                    while (wordBreakdown[firstPointer].equals(wordBreakdown[lastPointer-1]) && lastPointer>0){
+                        lastPointer--;
+                    }
+                    if(firstPointer == lastPointer){
+                        break;
+                    }
+                    System.out.println("After while, before decrement: " + lastPointer);
+                    wordBreakdown[firstPointer] = wordBreakdown[lastPointer-1];
+                    wordBreakdown[lastPointer-1] = temp;
+                    lastPointer--;
+                    firstPointer--;
+                    System.out.println(lastPointer);
+                    System.out.println(stringOfWords);
+                    for(int o = 0; o < wordBreakdown.length; o++){
+                        System.out.print(wordBreakdown[o] + " ");
+                    }
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+        String[] uniqueWords = new String[lastPointer];
+        for (int i = 0; i < lastPointer; i++){
+            uniqueWords[i] = wordBreakdown[i];
+        }
+        int[] counterOfUniqueWords = new int[uniqueWords.length];
+        for (String word: wordBreakdown) {
+            for (int i = 0; i < uniqueWords.length; i++){
+                if(word.equals(uniqueWords[i])){
+                    counterOfUniqueWords[i]++;
+                }
+            }
+        }
+        System.out.println(lastPointer);
+
+        for(int i = 0; i < uniqueWords.length; i++){
+            System.out.println(uniqueWords[i] + ": " + counterOfUniqueWords[i]);
+        }
+        System.out.println();
+
+    }
+
+    // 5 ON problems
 
 }
 
